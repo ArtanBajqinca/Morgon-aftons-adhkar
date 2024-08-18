c52ba286831349225e41515423328801ad2d1ce2
Arctan <artan.bajqinca@gmail.com>
2024-08-18 22:14:54 +0200

    add privacy policy to app info view

--- app/src/main/java/artan/bajqinca/morgon_afton_dhikr/views/InfoScreen.kt
+++ app/src/main/java/artan/bajqinca/morgon_afton_dhikr/views/InfoScreen.kt
@@ -1,7 +1,7 @@
 package artan.bajqinca.morgon_afton_dhikr.views
 
 import android.content.Intent
-import android.widget.Toast
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -15,8 +15,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,10 +41,29 @@ import artan.bajqinca.morgon_afton_dhikr.R
 import artan.bajqinca.morgon_afton_dhikr.font.AvenirFontFamily
 import artan.bajqinca.morgon_afton_dhikr.views.components.BackButton
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import java.util.Calendar
+import android.content.pm.PackageManager
+import android.util.Log
 
 @Composable
 fun InfoScreen(navController: NavController = rememberNavController()) {
     val context = LocalContext.current
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+    // Get the app version dynamically
+    val packageInfo = try {
+        context.packageManager.getPackageInfo(context.packageName, 0)
+    } catch (e: PackageManager.NameNotFoundException) {
+        Log.e("InfoScreen", "Package not found", e)
+        null
+    }
+    val appVersion = packageInfo?.versionName ?: "1.0"  // Fallback to "1.0" if versionName is not found
+
+
+    // Dialog state
+    val showDialog = remember { mutableStateOf(false) }
+    val dialogTitle = remember { mutableStateOf("") }
+    val dialogMessage = remember { mutableStateOf("") }
 
     Box(
         modifier = Modifier
@@ -92,7 +116,7 @@ fun InfoScreen(navController: NavController = rememberNavController()) {
                                     contentScale = ContentScale.Fit
                                 )
                                 Text(
-                                    text = "Version 1.0",
+                                    text = "Version $appVersion",  // Display the dynamic version
                                     color = colorResource(id = R.color.light_beige),
                                     style = TextStyle(
                                         fontFamily = AvenirFontFamily,
@@ -103,7 +127,76 @@ fun InfoScreen(navController: NavController = rememberNavController()) {
                                         .align(Alignment.CenterHorizontally)
                                         .padding(top = 15.dp)
                                 )
-                                Spacer(modifier = Modifier.height(40.dp))
+                                Spacer(modifier = Modifier.height(20.dp))
+
+                                // Developer Information
+                                Text(
+                                    text = "Developer: Artan Bajqinca",
+                                    color = colorResource(id = R.color.light_beige),
+                                    style = TextStyle(
+                                        fontFamily = AvenirFontFamily,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 16.sp
+                                    ),
+                                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                )
+                                Spacer(modifier = Modifier.height(30.dp))
+
+                                // Privacy Policy Link
+                                Box(
+                                    modifier = Modifier
+                                        .width(280.dp)
+                                        .height(30.dp)
+                                        .clip(RoundedCornerShape(10.dp))
+                                        .background(colorResource(id = R.color.light_beige))
+                                        .clickable {
+                                            // Show the dialog for Privacy Policy
+                                            dialogTitle.value = "Privacy Policy"
+                                            dialogMessage.value = "We respect your privacy. This app does not collect, store, or share any personal data. Your experience with the app is completely anonymous."
+                                            showDialog.value = true
+                                        },
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = "Privacy Policy",
+                                        style = TextStyle(
+                                            fontFamily = AvenirFontFamily,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 18.sp,
+                                            color = colorResource(id = R.color.gray)
+                                        )
+                                    )
+                                }
+
+                                Spacer(modifier = Modifier.height(20.dp))
+
+                                // Terms of Service Link
+                                Box(
+                                    modifier = Modifier
+                                        .width(280.dp)
+                                        .height(30.dp)
+                                        .clip(RoundedCornerShape(10.dp))
+                                        .background(colorResource(id = R.color.light_beige))
+                                        .clickable {
+                                            // Show the dialog for Terms of Service
+                                            dialogTitle.value = "Terms of Service"
+                                            dialogMessage.value = "We respect your privacy. This app does not collect, store, or share any personal data. Your experience with the app is completely anonymous."
+                                            showDialog.value = true
+                                        },
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = "Terms of Service",
+                                        style = TextStyle(
+                                            fontFamily = AvenirFontFamily,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 18.sp,
+                                            color = colorResource(id = R.color.gray)
+                                        )
+                                    )
+                                }
+
+                                Spacer(modifier = Modifier.height(20.dp))
 
                                 // Add a clickable Text to show OSS Licenses
                                 Box(
@@ -116,7 +209,7 @@ fun InfoScreen(navController: NavController = rememberNavController()) {
                                             val intent = Intent(context, OssLicensesMenuActivity::class.java)
                                             startActivity(context, intent, null)
                                         },
-                                    contentAlignment = Alignment.Center // Centers the content inside the Box
+                                    contentAlignment = Alignment.Center
                                 ) {
                                     Text(
                                         text = "Open Source Licenses",
@@ -129,6 +222,19 @@ fun InfoScreen(navController: NavController = rememberNavController()) {
                                     )
                                 }
 
+                                Spacer(modifier = Modifier.height(40.dp))
+
+                                // Copyright Information
+                                Text(
+                                    text = "© $currentYear Skyddad av upphovsrättslagen",
+                                    color = colorResource(id = R.color.light_beige),
+                                    style = TextStyle(
+                                        fontFamily = AvenirFontFamily,
+                                        fontWeight = FontWeight.Light,
+                                        fontSize = 14.sp
+                                    ),
+                                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                )
 
                                 Spacer(modifier = Modifier.height(40.dp))
                             }
@@ -138,6 +244,50 @@ fun InfoScreen(navController: NavController = rememberNavController()) {
             }
         }
     }
+
+    // Show the dialog if triggered
+    if (showDialog.value) AlertDialog(
+        onDismissRequest = { showDialog.value = false },
+        title = {
+            Text(
+                text = dialogTitle.value,
+                style = TextStyle(
+                    fontFamily = AvenirFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 23.sp,
+                    fontSize = 20.sp,
+                    color = colorResource(id = R.color.gray)
+                )
+            )
+        },
+        text = {
+            Text(
+                text = dialogMessage.value,
+                style = TextStyle(
+                    fontFamily = AvenirFontFamily,
+                    fontWeight = FontWeight.W600,
+                    lineHeight = 23.sp,
+                    fontSize = 16.sp,
+                    color = colorResource(id = R.color.gray)
+                )
+            )
+        },
+        confirmButton = {
+            Button(
+                onClick = { showDialog.value = false },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.gray)
+                )
+            ) {
+                Text(
+                    text = "OK",
+                    color = colorResource(id = R.color.light_beige)
+                )
+            }
+        },
+        shape = RoundedCornerShape(10.dp),
+        containerColor = colorResource(id = R.color.light_beige)
+    )
 }
 
 @Preview
--- app/src/main/java/artan/bajqinca/morgon_afton_dhikr/views/StartScreen.kt
+++ app/src/main/java/artan/bajqinca/morgon_afton_dhikr/views/StartScreen.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
